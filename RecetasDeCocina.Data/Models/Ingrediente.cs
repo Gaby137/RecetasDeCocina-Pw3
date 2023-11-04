4808ab26428e6aeef4b89ce928655810563f311f
Gaby137 <genrriquezsanchez@alumno.unlam.edu.ar>
2023-11-03 22:51:11 -0300

    se agrego las colecciones

---
+++ RecetasDeCocina.Data/Models/Ingrediente.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetasDeCocina.Data.Models;
+
+public  class Ingrediente
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    public string NombreDelIngrediente { get; set; }
+    public string CategoriaDelIngrediente { get; set; }
+    public string Descripcion { get; set; }
+    public string Gramos { get; set; }
+
+}
+//json { "_id": ObjectId("ingredient_id"),
+//"name": "nombre_del_ingrediente",
+//"category": "categoría_del_ingrediente",
+//"description": "Descripción del ingrediente" // Otros campos }
---
+++ RecetasDeCocina.Data/Models/PreferenciaUsuario.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecetasDeCocina.Data.Models;
+
+public  class PreferenciaUsuario
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    public ObjectId IdUsuario { get; set; }
+    public List<Ingrediente> IngredientesPreferidos { get; set; }
+    public List<Ingrediente> IngredientesDespreciados { get; set; }
+
+}
+//json { "_id": ObjectId("preference_id"),
+//"user_id": ObjectId("user_id"),
+//"liked_ingredients": ["zanahorias", "tomates"],
+//"disliked_ingredients": ["brócoli", "pimientos"] }
